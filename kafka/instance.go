59ed0319c4f66653e2584b9304bc74b581c95be8
Mari Barros <msb4@cin.ufpe.br>
2018-06-18 16:40:02 -0300

    Calculating and printing statistics

--- kafka/instance.go
+++ kafka/instance.go
@@ -2,17 +2,14 @@ package kafka
 
 import "sync"
 
-
 var instance *Kafka
 var once sync.Once
 
 func GetInstance() *Kafka {
 	once.Do(func() {
-		kafka := NewRandomKafka(25,100)
+		kafka := NewFromFile("kafka.txt")
 		kafka.Sort()
 		instance = &kafka
 	})
 	return instance
 }
-
-

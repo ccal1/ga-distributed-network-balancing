2d8f9a6de4ab904873afe546bdd93b6f710f6428
ccal1 <ccal@cin.ufpe.br>
2018-06-18 13:32:04 -0300

    feat(kafka.go): add from file and to file functions

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -4,6 +4,13 @@ import (
 	"math/rand"
 	"sort"
 	"time"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"io"
+	"encoding/json"
+	"strings"
+	"strconv"
 )
 
 type Kafka []Topic
@@ -15,12 +22,64 @@ func NewRandomKafka(partitions int, topics int) Kafka {
 	kafka := make([]Topic, topics)
 	for i := range kafka {
 		topicSize := rnd.Intn(1000)
-		kafka[i].Name = string(i)
+		kafka[i].Name = strconv.Itoa(i)
 		kafka[i].Partitions = make([]Partition, partitions)
 		partitions := kafka[i].Partitions
 		for j := range partitions {
 			partitions[j].Idx = j
 			partitions[j].Value = topicSize * rnd.Intn(100)
+		}
+	}
+	return kafka
+}
+
+func (k *Kafka) Write(w io.Writer) {
+	b, _ := json.Marshal(*k)
+	w.Write(b)
+}
+
+const(
+	topicSeparator = "SEPARATOR FOR TOPIC"
+	partitionSeparator = "PARTITION SEPARATOR"
+)
+
+func (k Kafka) ToFile(fileName string) error {
+	str := ""
+	str += fmt.Sprintf("%v %v\n", len(k), len(k[0].Partitions))
+	for _, topic := range k {
+		str += topicSeparator
+		str += topic.Name + "\n"
+		str += partitionSeparator
+		for _, partition := range topic.Partitions {
+			str += fmt.Sprintf("%v %v\n", partition.Idx, partition.Value)
+		}
+	}
+
+	return ioutil.WriteFile(fileName, []byte(str), os.ModePerm)
+}
+
+func NewFromFile(fileName string) Kafka {
+	bytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	str := string(bytes)
+	strs := strings.Split(str, topicSeparator)
+
+	var topicsSize, partitionsSize int
+	fmt.Sscanf(strs[0],"%d %d", &topicsSize, &partitionsSize)
+	kafka := make([]Topic, topicsSize)
+	for i, topic := range strs[1:] {
+		nameAndPartitions := strings.Split(topic, partitionSeparator)
+		kafka[i].Name = nameAndPartitions[0]
+		kafka[i].Partitions = make([]Partition, partitionsSize)
+		fmt.Println("Separate")
+
+		for j, partition := range strings.Split(nameAndPartitions[1], "\n") {
+			if partition == "" {
+				break
+			}
+			fmt.Sscanf(partition, "%d %d", &(kafka[i].Partitions[j].Idx), &(kafka[i].Partitions[j].Value))
 		}
 	}
 	return kafka
